7cb408e83cce9ea4b6bc3ffc4daeb3e640a34740
kulklex <kulklex@gmail.com>
2025-12-18 23:30:43 +0000

    Google login changes..

--- src/utils/axiosConfig.js
+++ src/utils/axiosConfig.js
@@ -2,7 +2,9 @@ import axios from 'axios';
 
 // Axios instance
 const api = axios.create({
-  baseURL: process.env.REACT_APP_API_URL || 'https://wig-store-server-production.up.railway.app/',
+  baseURL: process.env.REACT_APP_API_URL || 'https://wig-store-server-production.up.railway.app/' === "production"
+      ? "https://wig-store-server-production.up.railway.app/"
+      : "http://localhost:3300",
   timeout: 10000, 
   headers: {
     'Content-Type': 'application/json',
